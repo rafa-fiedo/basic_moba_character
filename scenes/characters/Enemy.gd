54d21b448be7236de0c0a6735d5dbed32edfa8e8
Rafa <fiedorowicz.rafal@gmail.com>
2020-12-11 13:11:31 +0100

    end of part_3 - collisions and ysort

--- scenes/characters/Enemy.gd
+++ scenes/characters/Enemy.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
 
-func _on_Enemy_mouse_entered():
+func _on_Area2D_mouse_entered():
 	Mouse.play_danger()
 
-func _on_Enemy_mouse_exited():
+func _on_Area2D_mouse_exited():
 	Mouse.reset()
